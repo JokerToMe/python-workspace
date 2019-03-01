49b99559b36e453c5680f2a81fa540682df4bf78
shinelaw <240272750@qq.com>
2019-03-01 16:29:57 +0800

    add new files

---
+++ faq.py
@@ -0,0 +1,13 @@
+# import os
+
+# print(os.path.abspath('.'))
+# C:\Users\Administrator\Desktop\python-workspace
+
+# print(__file__)
+# faq.py
+
+# import sys
+
+# print(sys.argv)
+# print(sys.executable)
+# C:\Users\Administrator\AppData\Local\Programs\Python\Python37-32\python.exe
